2f41594b917de52a7ab9e2ea10c28ffd42a01ca1
JovanyVale <gambino84_@hotmail.com>
2025-04-25 05:59:06 -0700

    Third commit

--- src/components/Coins.tsx
+++ src/components/Coins.tsx
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { useEffect } from "react"
 import { fetchData } from "../state/dataSlice"
 import type { AppDispatch, RootState } from "../state/store"
+import fortmatCurrency from "../helpers/formatCurrency"
 
 export default function Coins() {
 
@@ -14,13 +15,21 @@ export default function Coins() {
 
     console.log(data.content)
 
+
     return (
         <>
-            <main className="grid grid-cols-1 lg:grid-cols-3 auto-rows-max">
+            <main className="mt-10 max-w-11/12 mx-auto grid grid-cols-1 lg:grid-cols-3 auto-rows-max place-items-center gap-8">
 
                 {data.content.map(i => (
-                    <div key={i.id} className="w-8/12 ">
-                        <p>{i.name}</p>
+                    <div key={i.id} className="h-40 w-full greenCard place-content-center">
+                        <div className="flex items-center max-w-11/12">
+                            <img src={`https://assets.coincap.io/assets/icons/${i.symbol.toLowerCase()}@2x.png`} className="rounded-full max-w-full" />
+
+                            <div>
+                                <p>{i.name}</p>
+                                <p>{fortmatCurrency(Number(i.priceUsd))} USD</p>
+                            </div>
+                        </div>
                     </div>
                 ))}
 
---
+++ src/helpers/formatCurrency.ts
@@ -0,0 +1,17 @@
+export default function fortmatCurrency(amount: number) {
+    if (amount >= 1) {
+        return new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        }).format(amount);
+    } else {
+        return new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 8
+        }).format(amount);
+    }
+}
